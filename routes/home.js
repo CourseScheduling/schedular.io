0fc9c628e37a3ab4bc5eecce2f62100f4b10d6db
Joseph Thomas <joey6978@gmail.com>
2016-07-07 20:42:22 -0700

    Making a contact page

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,10 @@ var router = require('koa-router')();
 
 
 router.get('/',function (){
-	
 	this.render('home/home');
-	
+});
+router.get('/contact',function (){
+	this.render('home/contact');
 });
 
 
