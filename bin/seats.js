ca4dffca5d0f5c13f6a9cfed5e063dd714289d3e
Joseph Thomas <joey6978@gmail.com>
2016-07-15 14:35:43 -0700

    Added seat message type switcher

--- bin/seats.js
+++ bin/seats.js
@@ -1,5 +1,13 @@
 global.seats.on('message', function(m){
-	
+	switch(m.type){
+		case 'E': //Error
+			console.log('ERROR In The Seat Department');
+			console.log(m.data);
+		break;
+		case 'S': //Seats
+			global.seats = m.data;
+		break;
+	}
 });
 
 
