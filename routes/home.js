61a2255ea52738b840903ddbd61c402101b7ef13
Joseph Thomas <joey6978@gmail.com>
2016-07-28 11:05:14 -0700

    Changed title, and updated the about/contact pages

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,13 @@ var router = require('koa-router')();
 router.get('/',function (){
 	this.render('home/home');
 });
+
 router.get('/contact',function (){
 	this.render('home/contact');
+});
+
+router.get('/about',function (){
+	this.render('home/about');
 });
 
 
