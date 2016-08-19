ff64d5e2e6c576f9732ff13520846521ec480861
Joseph Thomas <joey6978@gmail.com>
2016-08-19 03:20:55 -0700

    Random commit

--- routes/auth/auth__api.js
+++ routes/auth/auth__api.js
@@ -1,80 +1,38 @@
 var router = require('koa-router')()
-var async = require('async')
-var Mongo = require('monk')
-var Token = require('../../bin/Token')
-
-var EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var USERNAME_REGEX = /[^(A-Z)(a-z)(0-9)_-]/g
-
-/**
- * The giant function object used for validation
- * @param {String} username
- * @param {String} email
- * @param {Function} good
- * @param {Function} bad
- */
-
-/**
- * The function used to check if the email and username is good
- * @param  {Object} inputs
- * @param  {Function} callback
- * @return
- */
-Validator.syntax = (email, username, callback) => {
-  var domain = email.split('@')[1]
-
-  if (!global.domainList[domain]) return callback('EMAIL DOMAIN')
-  if (!email.test(EMAIL_REGEX)) return callback('EMAIL INVALID')
-  if (username.test(USERNAME_REGEX)) return callback('USERNAME INVALID')
-
-  callback(null)
-}
-
-/**
- * The function used to query the database to check if it's good
- * @param  {String} email
- * @param  {String} username
- * @param  {Function} callback
- * @return
- */
-Validator.exists = (email, username, callback) => {
-  var _query = {
-    $or: [
-      {username: username},
-      {email: email}
-    ]
-  }
-
-  var _option = {_id: 1}
-
-  Mongo.get('User').find(_query, _option).then(function (data) {
-    if (!data.length) {
-      return callback(data[0])
-    }
-
-    if (data[0].username === username) callback('USERNAME TAKEN')
-    else callback('EMAIL TAKEN')
-  })
-}
+var Validator = require('./validate')
 
 /** The signup route */
 router.post('/api/v1/signup', function * () {
-  var _email = this.body.email
-  var _username = this.body.username
-  var _password = this.body.password
+
+  var _inputs = {
+    email: this.body.email,
+    username: this.body.username,
+    password: this.body.password
+  }
 
   yield new Promise(function (resolve, reject) {
-    Validator('signup', _username, _email, resolve, reject)
+    Validator('signup', _inputs).then((data) => {
+      Signup(_inputs)
+    }, (error) => {
+      reject(error)
+    })
   })
 })
 
 /** The login route */
 router.post('/api/v1/auth', function * () {
-  var _username = this.body.username
-  var _password = this.body.password
 
-  yield new Promise(function (resolve, reject){
-    Validator('login', _username, _password, resolve, reject)
+  var _inputs = {
+    username: this.body.username,
+    password: this.body.password
+  }
+
+  yield new Promise(function (resolve, reject) {
+    Validator('login', _inputs).then((data) => {
+      Auth(data)
+    }, (error) => {
+      reject(error)
+    })
   })
 })
 module.exports = router
--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,29 @@
 var router = require('koa-router')();
 
 
-router.get('/',function (){
-	this.render('home/home');
+router.get('/', () => {
+  this.render('home/home');
 });
 
-router.get('/contact',function (){
-	this.render('home/contact');
+router.get('/contact', () => {
+  this.render('home/contact');
 });
 
-router.get('/about',function (){
-	this.render('home/about');
+router.get('/about', () => {
+  this.render('home/about');
 });
 
+/** The following are the authentication views */
+router.get('/u/login', () => {
+  this.render('auth/login')
+})
+
+router.get('/u/signup', () => {
+  this.render('auth/signup')
+})
+
+router.get('/u/reset', () => {
+  this.render('auth/reset')
+})
 
 module.exports = router;
